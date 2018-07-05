68482e2b6a49c3e3bec4a85f0ee663744ea67a68
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-07-05 14:06:52 +0300

    Fixed python path

--- cloudshell-L1-{{cookiecutter.project_slug}}/install_driver.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/install_driver.bat
@@ -4,14 +4,20 @@ set DRIVER_FOLDER=%~dp0
 set DRIVER_NAME="{{cookiecutter.project_slug}}"
 set DRIVER_PYTHON="%DRIVER_FOLDER%\Scripts\python.exe"
 set PACKAGES="%DRIVER_FOLDER%\packages"
+set QS_PYTHON_PATH="%DRIVER_FOLDER%\..\..\python"
 
 set QS_PYTHON_REGEXP="^2.7.*"
 set QS_PYTHON=%1
 
 if not defined QS_PYTHON (
-    set QS_PYTHON="%DRIVER_FOLDER%\..\..\python"
-    for /F %%x in ('dir /b %QS_PYTHON%') do echo %%x|findstr %QS_PYTHON_REGEXP%>nul&&set QS_PYTHON=%QS_PYTHON%\%%x\python.exe
+    if exist %QS_PYTHON_PATH% ( for /F %%x in ('dir /b %QS_PYTHON_PATH%') do echo %%x|findstr %QS_PYTHON_REGEXP%>nul&&set QS_PYTHON=%QS_PYTHON_PATH%\%%x\python.exe&&goto INSTALL )
 )
+
+if not defined QS_PYTHON set QS_PYTHON="python.exe"
+
+:INSTALL
+
+echo "Python: %QS_PYTHON%"
 
 if not exist %DRIVER_PYTHON% %QS_PYTHON% -m virtualenv --system-site-packages "%DRIVER_FOLDER%\"
 if exist %PACKAGES% %DRIVER_PYTHON% -m pip install -r "%DRIVER_FOLDER%\requirements.txt" --no-index -f %PACKAGES%
