0bd2c9aab6e022f7a252313c951374fef1feb713
Yaroslav Nikonorov <yaroslav.n@quali.com>
2018-09-03 18:58:55 +0300

    Added update pip to install_driver.bat, dded INSTALL.txt

--- cloudshell-L1-{{cookiecutter.project_slug}}/install_driver.bat
+++ cloudshell-L1-{{cookiecutter.project_slug}}/install_driver.bat
@@ -20,6 +20,7 @@ if not defined QS_PYTHON set QS_PYTHON="python.exe"
 echo "Python: %QS_PYTHON%"
 
 if not exist %DRIVER_PYTHON% %QS_PYTHON% -m virtualenv --system-site-packages "%DRIVER_FOLDER%\"
+%DRIVER_PYTHON% -m pip install --upgrade pip
 if exist %PACKAGES% %DRIVER_PYTHON% -m pip install -r "%DRIVER_FOLDER%\requirements.txt" --no-index -f %PACKAGES%
 if not exist %PACKAGES% %DRIVER_PYTHON% -m pip install -r "%DRIVER_FOLDER%\requirements.txt"
 copy "%DRIVER_FOLDER%\driver_exe_template" "%DRIVER_FOLDER%\..\%DRIVER_NAME%.exe"
